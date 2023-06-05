cb923e202102c7cb009e1913064e4c1f8dc635f5
Vega! <78299312+kinguser981@users.noreply.github.com>
2023-06-06 01:59:51 +0330

    Update omni_CAP_sprout.mk

--- device/hmd/CAP_sprout/omni_CAP_sprout.mk
+++ device/hmd/CAP_sprout/omni_CAP_sprout.mk
@@ -9,11 +9,14 @@
 PRODUCT_RELEASE_NAME := CAP_sprout
 
 # Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit from CAP_sprout device
 $(call inherit-product, device/hmd/CAP_sprout/device.mk)
